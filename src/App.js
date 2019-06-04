6b8a851fb9db14b79d2fd868d8864807f03d8fc6
Jeffrey Yeung <jeffyyw@gmail.com>
2019-06-04 17:34:47 -0400

    api return rendered to page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import Header from './Header.js'
 
 class App extends Component {
   constructor() {
     super();
     
-    this.state = {
-      userInput: "",
-      userSearch: ""
-    }
-  }
-
-  handleChange = event => {
-    this.setState({
-      userInput: event.target.value
-    })
-  }
-
-  handleSearch = event => {
-    event.preventDefault();
-    const userQuery = this.state.userInput;
-    this.setState({
-      userSearch: userQuery 
-    }, () => {
-      this.searchShows(this.state.userSearch)
-    })
-  }
-
-  searchShows = (userSearch) => {
-    axios({
-      method: 'GET',
-      url: "http://api.tvmaze.com/search/shows",
-      dataResponse: 'json',
-      params: {
-        q: userSearch
-      }
-    }).then(results => {
-      console.log(results)
-    }).catch(error => {
-      console.log('error')
-    })
   }
 
   render() {
     return (
       <div className="App">
-        <form action="">
-          <label htmlFor="searchBar"></label>
-          <input type="text" id="searchBar" onChange={this.handleChange}></input>
-          <input type="submit" onClick={this.handleSearch} />
-        </form>
+        <Header />
+
+
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Header extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            userInput: "",
+            userSearch: "",
+            showsArray: []
+        }
+    }
+
+    handleChange = event => {
+        this.setState({
+            userInput: event.target.value
+        })
+    }
+
+    handleSearch = event => {
+        event.preventDefault();
+        const userQuery = this.state.userInput;
+        this.setState({
+            userSearch: userQuery
+        }, () => {
+            this.searchShows(this.state.userSearch)
+        })
+    }
+
+    searchShows = (userSearch) => {
+        axios({
+            method: 'GET',
+            url: "http://api.tvmaze.com/search/shows",
+            dataResponse: 'json',
+            params: {
+                q: userSearch
+            }
+        }).then(results => {
+            const showData = results.data
+
+            this.setState({
+                showsArray: showData
+            })
+
+            // console.log(this.state.showsArray)
+        }).catch(error => {
+            console.log('error')
+        })
+    }
+
+
+
+    render() {
+        return (
+            <header className="wrapper">
+                <form action="">
+                    <label htmlFor="searchBar"></label>
+                    <input type="text" id="searchBar" onChange={this.handleChange}></input>
+                    <input type="submit" onClick={this.handleSearch} />
+                </form>
+                <div className="showLists">
+                    <div className="showResults">
+
+                        { this.state.showsArray.map((item) => {
+                            return <img src={item.show.image.original}/>
+                        }) }
+                        
+                    </div>
+                    <div className="listCreator">
+
+                    </div>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
