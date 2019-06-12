529a896a53c0bc02c2eeb6f0955c7441c027d0a2
andreamaille <andrea.maille@gmail.com>
2019-06-11 21:08:45 -0400

    updated labels and click to add button

--- src/Header.js
+++ src/Header.js
@@ -341,7 +341,7 @@ class Header extends Component {
                         </div>
                         
                         <form action="" className="searchForm">
-                            <label htmlFor="searchBar"></label>
+                            <label htmlFor="searchBar" className="visuallyHidden">Search for TV shows</label>
                             <input  
                                 id="searchBar" 
                                 onChange={this.handleChange}
@@ -400,7 +400,7 @@ class Header extends Component {
                                             <button
                                                 className="clickClose"
                                                 onClick={this.closeModal}>
-                                                X
+                                                &#xd7;
                                             </button>
                                         </div>
                                     </div>
@@ -424,7 +424,7 @@ class Header extends Component {
                                             value={this.state.userSubmitTitle}
                                             placeholder="Name Your List"
                                         />
-                                        <label htmlFor="userListTitle"></label>
+                                        <label htmlFor="userListTitle" className="visuallyHidden">Name your TV show list</label>
                                         <input
                                             type="submit"
                                             value="Submit List"
